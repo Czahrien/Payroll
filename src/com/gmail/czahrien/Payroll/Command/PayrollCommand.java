e254d47c61b5fa0a4b33fa226478d3635b1f48f8
Ben <bdm8233@rit.edu>
2012-06-11 20:50:28 -0400

    More work at implementing PayrollCommand.

--- src/com/gmail/czahrien/Payroll/Command/PayrollCommand.java
+++ src/com/gmail/czahrien/Payroll/Command/PayrollCommand.java
@@ -1,24 +1,56 @@
 package com.gmail.czahrien.Payroll.Command;
 
 import com.gmail.czahrien.Payroll.Plugin.*;
+import com.gmail.czahrien.Payroll.Storage.PayrollEntry;
+import com.gmail.czahrien.Payroll.Storage.PayrollStorage;
+import com.gmail.czahrien.Payroll.util.Paginate;
+import com.gmail.czahrien.Payroll.util.PayrollUtil;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.TreeMap;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PayrollCommand implements CommandExecutor {
 
     PayrollPlugin myPlugin;
+    PayrollStorage myPayrollStorage;
     
     public PayrollCommand(PayrollPlugin p) {
         myPlugin = p;
+        myPayrollStorage = PayrollStorage.getInstance(p);
     }
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
             switch(cmd.getName().toLowerCase()) {
                 case "payroll":
-                    if(sender.hasPermission("Payroll.user") && args.length >= 1) {
-                        // TODO: List payrolls user is a part of.
+                    if(sender.hasPermission("Payroll.user")) {
+                        if(!(sender instanceof Player)) {
+                            sender.sendMessage(ChatColor.RED + "This command is only valid for players!");
+                            return true;
+                        }
+                        int page = 1;
+                        if(args.length > 1) {
+                            try {
+                                page = Integer.parseInt(args[1]);
+                            } catch(NumberFormatException e) {
+                                sender.sendMessage(ChatColor.RED + "Invalid page number: " + args[1]);
+                            }
+                        }
+                        Collection<PayrollEntry> v = myPayrollStorage.myPayrolls.values();
+                        LinkedList<PayrollEntry> usersPayrolls = new LinkedList<>();
+                        for(PayrollEntry e : v) {
+                            if(e.myPlayers.contains(sender.getName())) {
+                                usersPayrolls.add(e);
+                            }
+                        }
+                        Paginate.paginator(usersPayrolls, sender, page, 5);
                         return true;
                     }
                     break;
@@ -26,22 +58,64 @@ public class PayrollCommand implements CommandExecutor {
                     if(sender.hasPermission("Payroll.admin") && args.length >= 1) {
                         switch(args[0]) {
                             case "ls":
-                                // TODO: List (pages of 5) payrolls.
-                                break;
+                                int page = 1;
+                                if(args.length > 1) {
+                                    try {
+                                        page = Integer.parseInt(args[1]);
+                                    } catch(NumberFormatException e) {
+                                        sender.sendMessage(ChatColor.RED + "Invalid page number: " + args[1]);
+                                    }
+                                }
+                                Paginate.paginator(myPayrollStorage.myPayrolls.values(), sender, page, 5);
+                                return true;
                             case "add":
-                                // TODO: Create new payroll.
+                               if(args.length > 2) {
+                                   //TODO: Annoying things I don't want to do at the moment.
+                               }
                                 break;
                             case "adduser":
-                                // TODO: Add user to existing payroll
+                                if(args.length == 3) {
+                                    PayrollEntry e = myPayrollStorage.myPayrolls.get(args[1]);
+                                    if(e == null) {
+                                        sender.sendMessage(ChatColor.RED + "No such payroll: " + args[1]);
+                                        return false;
+                                    }
+                                    Player p = Bukkit.getPlayer(args[2]);
+                                    if(p == null) {
+                                        sender.sendMessage(ChatColor.RED + "No such player: " + args[2]);
+                                        return false;
+                                    }
+                                    e.addPlayer(p);
+                                    sender.sendMessage(ChatColor.AQUA + args[2] + " removed added successfully to " + args[1] + "!");
+
+                                    return true;
+                                } 
                                 break;
                             case "rm":
                                 // TODO: Remove payroll.
                                 break;
                             case "rmuser":
-                                // TODO: Remove a user from payroll.
+                                if(args.length == 3) {
+                                    PayrollEntry e = myPayrollStorage.myPayrolls.get(args[1]);
+                                    if(e == null) {
+                                        sender.sendMessage(ChatColor.RED + "No such payroll: " + args[1]);
+                                        return false;
+                                    }
+                                    Player p = Bukkit.getPlayer(args[2]);
+                                    if(p == null) {
+                                        sender.sendMessage(ChatColor.RED + "No such player: " + args[2]);
+                                        return false;
+                                    }
+                                    if(e.removePlayer(p)) {
+                                        sender.sendMessage(ChatColor.AQUA + args[2] + " removed successfully from " + args[1] + "!");
+                                    } else {
+                                        sender.sendMessage(ChatColor.RED + args[2] + " is not in the payroll " + args[1] + "!" );
+                                    }
+                                    return true;
+                                } 
                                 break;
                             case "set":
-                                // TODO: set <payroll> <option> <arg>
+                                // TODO: set <payroll> <option> <arg(s)>
                                 break;
                         }
                     }
--- src/com/gmail/czahrien/Payroll/Plugin/PayrollPlugin.java
+++ src/com/gmail/czahrien/Payroll/Plugin/PayrollPlugin.java
@@ -1,11 +1,13 @@
 package com.gmail.czahrien.Payroll.Plugin;
 
 import com.gmail.czahrien.Payroll.Command.*;
+import com.gmail.czahrien.Payroll.Storage.PayrollStorage;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class PayrollPlugin extends JavaPlugin {
     private PayrollCommand myCommands;
+    private PayrollStorage myStorage;
 
     @Override
     public void onEnable() {
@@ -17,8 +19,8 @@ public class PayrollPlugin extends JavaPlugin {
             }
         }, 0L, 1200L);
         
+        myStorage = PayrollStorage.getInstance(this);
         myCommands = new PayrollCommand(this);
-        
         getCommand("payroll").setExecutor(myCommands);
         getCommand("payrolladmin").setExecutor(myCommands);
     }
